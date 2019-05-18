4d6f58523414ca0cc5ae37e5e643848e05dbc035
johnadamsii <johnadams@mail.usf.edu>
2019-05-17 20:01:16 -0400

    error handling mostly done, return to previous commit for threading code and other stuff

--- randomGenerator.cpp
+++ randomGenerator.cpp
@@ -1,7 +1,6 @@
 //! Run with: g++ randomGenerator.cpp -o randGen && ./randGen
 
 #include <iostream>
-#include <vector>
 #include <ctime>
 #include <cstdlib>
 #include <string>
@@ -14,7 +13,6 @@
 
 //! constants
 const char *argv0;
-//static const unsigned int SLEEP_TIME = 100'000;
 
   //* TO DO:
         //* Error handling command line args
@@ -23,13 +21,13 @@ const char *argv0;
         //* support variable len vecs and variable number vecs 
         //* support doubles (decimal numbers) by making templated function?
         //* move main to different file
-        //* need to split into 4 threads of 25 and use mutithreading
+        //* need to split into n threads of n/4 and use mutithreading
 
 using namespace std;
 
 //! function declarations
-vector<unsigned int> getRandomVec(unsigned int randVecLen, unsigned int randNumUpperBound, unsigned int randNumLowerBound);
-int randNumGenerator(unsigned int upper, unsigned int lower) { return rand()% (upper + 1 - lower) + lower; }
+void printRandomVec(long unsigned int randVecLen, long int randNumUpperBound, long int randNumLowerBound);
+int randNumGenerator(long int upper, long int lower) { return rand()% (upper + 1 - lower) + lower; }
 
 
 void usage(FILE *f, int err) 
@@ -47,71 +45,60 @@ int main(int argc, const char *argv[])
     argv0 = argv[0];
 
     clock_t tStart = clock();
-    
-    srand(time(NULL));
+    srand(time(NULL)); // seed for RNG
+
+    if (argv[1] == "--help" || argv[1] == "-h")
+        usage(stdout, 0);
 
     if (argc != 5)
+        usage(stderr, 1);
+
+    int argIndex;
+    try {
+            for (argIndex = 1; argIndex < argc; argIndex++)
+                int temp(stoi(argv[argIndex])) ;
+        } 
+    catch (invalid_argument &e) 
     {
+        fprintf(stderr, "Could not parse %s as an integer\n", argv[argIndex]);
+        exit(1);
+    }
+
+    static const long int NUM_VECS = stoi(argv[1]);
+    static const long int LEN_VECS = stoi(argv[2]);
+    static const long int LOWER_BOUND = stoi(argv[3]);
+    static const long int UPPER_BOUND = stoi(argv[4]);
+
+    if (NUM_VECS < 0 || LEN_VECS < 0)
+    {
+        cout << "NUM_VECS and LEN_VECS must be non-negative\n";
         usage(stderr, 1);
     }
 
-     if (argv[1] == "--help" || argv[1] == "-h")
-        //usage(stdout, 0);
-        cout << "ARGS";
+    if (LOWER_BOUND > UPPER_BOUND)
+    {
+        cout << "MIN cannot be greater than max\n";
+        usage(stderr, 1);
+    }
 
-    //  if (argc == 1)
-    //  {
-    //       int random_number = rand()% (10000 + 1 - 0) + 0;
-    //       cout << random_number;
-    //       exit(0);
-    //  }
-
-    static const unsigned int NUM_VECS = stoi(argv[1]);
-    static const unsigned int LEN_VECS = stoi(argv[2]);
-    static const unsigned int LOWER_BOUND = stoi(argv[3]);
-    static const unsigned int UPPER_BOUND = stoi(argv[4]);
-
-    vector<unsigned int> randomNums;
     cout << KGRN; //* set color
     
-    for (unsigned int i = 0; i < NUM_VECS; i++)
+    for (long int i = 0; i < NUM_VECS; i++)
     {
-        randomNums = getRandomVec(LEN_VECS,UPPER_BOUND,LOWER_BOUND);
-        for (auto i: randomNums)
-            cout << i;
-       
-        // usleep(SLEEP_TIME); //! Sleep to slow down printing
+        printRandomVec(LEN_VECS,UPPER_BOUND,LOWER_BOUND);
         cout << '\n';  
     }
 
     cout << RST; //* reset color
 
-    cout << "Generated " << NUM_VECS << " random vectors of length " << LEN_VECS << " in: ";
+    cout << "\nGenerated " << NUM_VECS << " random vectors of length " << LEN_VECS << " in: ";
     printf("%.4fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
 
-    //int numThreads = std::thread::hardware_concurrency();
-    //cout << "Concurrent threads supported: " << numThreads << endl;
-    
     return 0;
 }
 
-vector<unsigned int> getRandomVec(unsigned int randVecLen, unsigned int randNumUpperBound, unsigned int randNumLowerBound)
+void printRandomVec(long unsigned int randVecLen, long int randNumUpperBound, long int randNumLowerBound)
 {
-    vector<unsigned int> randomNums;
-    unsigned int randNum;
-
-    for (unsigned int i = 0; i < randVecLen; i++) 
-    {
-        randNum = randNumGenerator(randNumUpperBound,randNumLowerBound);
-        randomNums.emplace_back(randNum);
-    }
-
-    return randomNums; 
+    for (long unsigned int i = 0; i < randVecLen; i++) 
+        cout << randNumGenerator(randNumUpperBound,randNumLowerBound);
 }
-
-//! run $ /usr/bin/time ./randGem      to get execution time on UNIX
-
-
-
-
-
